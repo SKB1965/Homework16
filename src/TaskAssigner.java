5ca0ce58758a5bbf9c4f9c6fa0769f81906c145b
Stanley <skbeyan@dmacc.edu>
2019-04-07 22:14:54 -0500

    initial commit

---
+++ src/TaskAssigner.java
@@ -0,0 +1,38 @@
+package edu.dmacc.codedsm.hw16;
+
+import java.util.objects;
+
+public class TaskAssigner {
+
+    private String toDO;
+    private User email;
+    private User phone;
+
+    public TaskAssigner(String toDo, User email, User phone){
+        this.toDO = toDO;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public static void main(String[] args) {
+
+        "stanleykbeyan2005@yahoo.com"
+                "Greetings"
+                        data: "{"Greetings from me to you"};
+            "to": [{"email"; "forkpawu74@gmail.com"]]}];
+    "from": {"email"; "stanleykbeyan2005@yahoo.com"};
+                "subject"; "Greetings";
+                "content": [{"type"; "text", "value", "good"];
+
+    }
+
+    public static void main(String)[] args){
+
+    String country = my.newCountry();
+    System.out.println(Country);
+        }
+        public String get Country(){
+    return "Liberia";
+
+        }
+}
